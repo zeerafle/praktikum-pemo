673ecfe5db2227511e6aaac51beeba90f1cdf203
Sam Fareez <vsefareez@gmail.com>
2022-05-28 13:31:13 +0800

    pertemuan 7

---
+++ Project Pertemuan/praktikum-pertemuan-7/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'main_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.orange),
+      home: MainPage(),
+    );
+  }
+}
---
+++ Project Pertemuan/praktikum-pertemuan-7/lib/main_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:pertemuan7_crud/item_card.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class MainPage extends StatelessWidget {
+  final TextEditingController nameController = TextEditingController();
+  final TextEditingController ageController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    FirebaseFirestore firestore = FirebaseFirestore.instance;
+    CollectionReference users = firestore.collection("users");
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Firestore Demo',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        body: Stack(
+          children: [
+            ListView(
+              children: [
+                //// VIEW DATA HERE
+                FutureBuilder<QuerySnapshot>(
+                  future: users.get(),
+                  builder: (_, snapshot) {
+                    return (snapshot.hasData)
+                        ? Column(
+                            children: snapshot.data!.docs
+                                .map(
+                                  (e) => ItemCard(e.get('name'), e.get('age')),
+                                )
+                                .toList(),
+                          )
+                        : Text('Loading...');
+                  },
+                ),
+                SizedBox(height: 150)
+              ],
+            ),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 15),
+                  decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                    BoxShadow(
+                        color: Colors.black12,
+                        offset: Offset(-5, 0),
+                        blurRadius: 15,
+                        spreadRadius: 3)
+                  ]),
+                  width: double.infinity,
+                  height: 130,
+                  child: Row(
+                    children: [
+                      SizedBox(
+                        width: MediaQuery.of(context).size.width - 160,
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            TextField(
+                              controller: nameController,
+                              decoration: InputDecoration(hintText: "Name"),
+                            ),
+                            TextField(
+                              controller: ageController,
+                              decoration: InputDecoration(hintText: "Age"),
+                              keyboardType: TextInputType.number,
+                            ),
+                          ],
+                        ),
+                      ),
+                      Container(
+                        height: 130,
+                        width: 130,
+                        padding: const EdgeInsets.fromLTRB(15, 15, 0, 15),
+                        child: ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                            ),
+                            child: Text(
+                              'Add Data',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            onPressed: () {
+                              // ADD DATA HERE
+                              users.add({
+                                'name': nameController.text,
+                                'age': int.tryParse(ageController.text) ?? -1,
+                              });
+
+                              nameController.text = '';
+                              ageController.text = '';
+                            }),
+                      )
+                    ],
+                  ),
+                )),
+          ],
+        ));
+  }
+}
